68fc65de228202623927eea82186d8d51f9bd2b7
Jack Ellis <Jack@Administrators-MacBook-Pro.local>
2017-01-20 10:12:14 +0000

    Make props reactive so they can be updated outside of the parent element

--- test/index.js
+++ test/index.js
@@ -5,35 +5,25 @@ global.window = global.document.defaultView;
 var vuenit = require('../lib');
 vuenit.vuePath = 'vue/dist/vue';
 
-var directive = {
-  bind(el, binding){
-    el.innerHTML = [binding.value, binding.arg, JSON.stringify(binding.modifiers)].join(', ');
+var component = {
+  template : '<div> <slot name="header"></slot> <div>{{something}}</div> <slot></slot> <slot name="footer"></slot> </div>',
+  props : ['something']
+};
+var options = {
+  name : 'test-component',
+  innerHTML : '<div slot="header">I am a header</div><div slot="footer">I am a footer</div><div>I am content</div>',
+  props : {
+    something : 'I am a prop'
   }
 };
-var vm = vuenit.directive({test : directive}, {
-  props : {x : 2, y : 4},
-  expression : 'x + y',
-  argument : 'fred',
-  modifiers : ['bob']
-});
+
+var vm = vuenit.component(component, options);
+
 console.log(vm.$el.outerHTML);
 
-var props = {'show-something-here' : false};
-var vm = vuenit.directive('if', {
-  props : props,
-  expression : "showSomethingHere"
-});
-console.log(vm.$el.outerHTML);
-vm.$parent.showSomethingHere = true;
-vm.$nextTick(() => {
+vm.data.something = 'I am changed';
+console.log(vm.something);
+vm.$nextTick(function () {
+  console.log(vm.something);
   console.log(vm.$el.outerHTML);
-  // done();
 });
-
-
-var vm2 = vuenit.directive('for', {
-  props : {list : [1, 2, 3, 4]},
-  expression : "l in list",
-  template : '<div><div v-directive>{{l}}</div></div>'
-});
-console.log(vm2.$el.outerHTML);
